bfc1d569a8cd86417f979124293da325b2ea8109
miyukic <m.yukic10@gmail.com>
2021-10-11 16:13:31 +0900

    プリプロセッサテスト

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
-#include <cstdio>
-#include <ctime>
-#include <iostream>
+#define calc(x, y) ((x) * (y))
+#define tozero(z) (0)
 
-const int val = 1;
-extern const int ext_val = 0x1234; 
-const char *p_val = "aiueo";
-extern const time_t time_val = std::time(NULL);
-
-//extern const int sub_val = 21;
-
-extern int sub_val;
-
-extern constexpr int I = 0xEEEE;
-
-constinit int coninit_val = 0x0000FFFF;
-
-int f();
 
 int main() {
-    //printf("%p\n", &val);
-    //printf("%d\n", ext_val);
-    //printf("%s\n", p_val);
-    //printf("%ld\n", time_val);
-    //printf("%d\n", I);
-    std::cout << sub_val << std::endl;
-    std::cout << f() << std::endl;
-
+    int a = calc(3, 5);
+    return tozero(a);
 }
 
-//constexpr int f() {
-//    return 0;
-//}
 

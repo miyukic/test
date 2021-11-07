8d268709f05e8748bee8c3899d069850a0d50aee
miyukic <m.yukic10@gmail.com>
2021-11-07 13:51:45 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-#define calc(x, y) ((x) * (y))
-#define tozero(z) (0)
-#define REP(i,a,b) for (int i = (a); i < b; ++i)
-#define dbg(...) std::cout << #__VA_ARGS__ << " = " << [&]{ return __VA_ARGS__; }() << std::endl;
+#include <iostream>
 
 
+inline uint32_t add(const uint32_t&, const uint32_t&);
+inline uint32_t sub(const uint32_t&, const uint32_t&);
 
 int main() {
-    int a = calc(3, 5);
-    return tozero(a);
+    //std::cout << "add(3, 5)=" << add(3, 5) << std::endl;
+    //std::cout << "sub(10, 5)=" << sub(323, 5) << std::endl;
 }
 
+inline uint32_t add(const uint32_t &a, const uint32_t &b) {
+    return a + b;    
+}
 
+inline uint32_t sub(const uint32_t &a, const uint32_t &b) {
+    uint32_t t = ~b;
+    t++;
+    return add(a, t);
+}

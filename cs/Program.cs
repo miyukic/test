a7efde57a7b50ff03827f2b1c54da93571e2b819
miyukic <m.yukic10@gmail.com>
2021-12-28 12:41:41 +0900

    トップレベルステートメントに実験

--- cs/Program.cs
+++ cs/Program.cs
@@ -2,18 +2,26 @@
 using System;
 using System.Linq.Expressions;
 
-class Program {
-    public static void Main() {
-        // 5+7*3
-        Expression body = Expression.Add(Expression.Constant(5),
-                Expression.Multiply(Expression.Constant(7),
-                    Expression.Constant(3)
-                    )
-                );
+Console.WriteLine("aiueo");
 
-        Expression<Func<int>> lambda = Expression.Lambda<Func<int>>(body);
-        Func<int> func = lambda.Compile();
-       Console.WriteLine(func());
+class Program {
+    public static int Main(String[] args) {
+        return 0;       
     }
 }
+
+//class Program {
+//    public static void Main() {
+//        // 5+7*3
+//        Expression body = Expression.Add(Expression.Constant(5),
+//                Expression.Multiply(Expression.Constant(7),
+//                    Expression.Constant(3)
+//                    )
+//                );
+//
+//        Expression<Func<int>> lambda = Expression.Lambda<Func<int>>(body);
+//        Func<int> func = lambda.Compile();
+//       Console.WriteLine(func());
+//    }
+//}
 

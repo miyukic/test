bd3881bf5c864ea2845aee704de7d67252db7dd5
miyukic <m.yukic10@gmail.com>
2021-12-29 12:17:11 +0900

    update

---
+++ b/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <ctime>
+
+int function() {
+    return 2;
+}
+
+int gVal = function();
+std::time_t cTime = std::time(NULL);
+
+int main() {
+    std::puts("みゆきちゃん");
+    return 1;
+}

0d289996848259f0d70aab63220493b4af777412
miyukic <m.yukic10@gmail.com>
2022-01-31 00:15:08 +0900

    update

---
+++ cs/msgroop/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks;
+using System.Threading;
+
+Task.Run( () => {
+});
+
+Console.WriteLine("Hello, World!");
+string? input = Console.ReadLine();
+Console.WriteLine(input);
+
+int GetMessage(Message msg) {
+    if (msg.id == 0) {
+        Thread.Sleep();
+    }
+    return -1;
+}
+
+void Dispatch(Message msg) {
+
+}
+
+int check = 0;
+while (check = GetMessage(msg)) {
+    if (check == -1) break;
+    Dispatch(in msg);
+}
+
+class Message {
+    public int id;
+}

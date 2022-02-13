77f5d9cf12a65bcc18276fbecc9171ba354adedf
miyukic <m.yukic10@gmail.com>
2022-02-13 19:59:29 +0900

    update

--- test.cpp
+++ test.cpp
@@ -6,6 +6,8 @@
 
 #include "test.hpp"
 
+#define dbg(...) std::cout << #__VA_ARGS__ << " = " << [&]{ return __VA_ARGS__; }() << std::endl;
+
 using namespace std::literals::string_literals;
 
 int counter = 0;
@@ -14,29 +16,57 @@ constexpr int FPS = 60;
 void onTimer(void);
 void onDraw(void);
 
-const  std::string Console::ESC = "\u001B";
 
-void Console::clear() {
-    std::cout << ESC + "1J\n"s;
-}
 
-void moveCursor(const int row, const int colum) {
-    std::cout << "\u001B[" << row << ";" << colum << "H\n";
-}
 
-void hideCursor() {
-    std::cout << "\u001B[>5h";
-}
+int x[] = {
+    2, 3, 4
+};
+
+
+int nums[][4] = {
+    {1, 2, 3, 5},
+    {1, 2, 3, 5},
+    {1, 2, 3, 5},
+    {1, 2, 3, 5}
+};
+
+
+//const std::string Console::ESC = "\u001B";
+
+//int Console::field[][10] =  {
+//    {2, 3},
+//    {2, 3},
+//    {2, 3},
+//    {2, 3},
+//    {2, 3}
+//};
+
+//Console::Console() {}
+
+//void Console::clear() {
+//    std::cout << ESC + "1J\n"s;
+//}
+
+//inline void Console::moveCursor(const int row, const int colum) {
+//    std::cout << "\u001B[" << row << ";" << colum << "H\n";
+//}
+
+//void Console::hideCursor() {
+//    std::cout << "\u001B[>5h";
+//}
 
 int main(int argc, char* argv[]) {
     std::cout << argv[0] << std::endl;
     const std::unique_ptr<std::string> message = std::make_unique<std::string>("引数なしで実行します。"s);
     if (argc <= 1) {
-        std::cout << *message << std::endl;
+        //std::cout << *message << std::endl;
+        dbg(*message)
     }
 
-    for (int i = 1; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
+    for (int i = 0; i < argc; i++) {
+        //std::cout << argv[i] << std::endl;
+        dbg(argv[i])
     }
     
     while (true) {
@@ -44,6 +74,7 @@ int main(int argc, char* argv[]) {
         onTimer();
     }
 }
+
 void onDraw() {
     Console::clear();
     Console::moveCursor(0, 0);
--- test.hpp
+++ test.hpp
@@ -1,13 +1,62 @@
+#pragma once
 #include <string>
+#include <iostream>
+
+using namespace std::literals::string_literals;
+
+//class Console {
+//    static const std::string ESC;
+//    static int field[][10];
+//
+//public:
+//    Console();
+//    static void clear();
+//
+//    static void moveCursor(const int row, const int colum);
+//
+//    static void hideCursor();
+//}; 
 
 class Console {
-    const static std::string ESC;
-    //int** mMap = new int*[10];
-    int* map[10] = {};
+    static const std::string ESC;
+    static int field[][10];
+
 public:
-    static void clear();
+//    Console();
 
-    static void moveCursor(const int row, const int colum);
+//    static void clear();
+//
+//    static void moveCursor(const int row, const int colum);
+//
+//    static void hideCursor();
 
-    static void hideCursor();
+    inline void clear() {
+        std::cout << ESC + "1J\n"s;
+    }
+
+    inline void moveCursor(const int row, const int colum) {
+        std::cout << "\u001B[" << row << ";" << colum << "H\n";
+    }
+
+    inline void hideCursor() {
+        std::cout << "\u001B[>5h";
+    }
+
 }; 
+
+inline const std::string Console::ESC = "\u001B";
+
+inline int Console::field[][10] =  {
+    {2, 3},
+    {2, 3},
+    {2, 3},
+    {2, 3},
+    {2, 3}
+};
+
+
+template <typename T>
+class HogeHoge {
+    static T foo = 20;
+};
+

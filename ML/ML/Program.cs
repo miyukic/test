372a7a04be853d74f0dbb3450cb186bac299e2cf
miyukic <m.yukic10@gmail.com>
2022-02-14 10:29:51 +0900

    単層パーセプトロン

--- ML/ML/Program.cs
+++ ML/ML/Program.cs
@@ -1,11 +1,62 @@
-﻿using System;
+﻿#if NET6_0
+#nullable enable
+
+using System;
 using Myk.Lib;
 
 
-
 namespace Myk {
+    namespace Utill {
+        #region Experimental Code
+        class Seconds {
+            int value;
+            public Seconds(int v) {
+                this.value = v;
+            }
+            public static implicit operator Minutes(Seconds val) {
+                return new Minutes(val.value / 60);
+            }
+
+            public static implicit operator Hours(Seconds val) {
+                return new Seconds(val.value / 60);
+            }
+        }
+        
+        class Minutes {
+            int value;
+            public Minutes(int v) {
+                this.value = v;
+            }
+
+            public static implicit operator Hours(Minutes val) {
+                return new Hours(val.value / 60);
+            }
+
+            public static implicit operator Seconds(Minutes val) {
+                return new Seconds(val.value * 60);
+            }
+        }
+
+        class Hours {
+            int value;
+            public Hours(int v) {
+                this.value = v;
+            }
+
+            public static implicit operator Seconds(Hours val) {
+                return new Seconds(val.value / 60 / 60);
+            }
+
+            public static implicit operator Minutes(Hours val) {
+                return new Minutes(val.value / 60);
+            }
+
+        }
+        #endregion
+    }
     namespace Lib {
 
+#region Matrix
         //(x1, x2)
         public class Matrix1x2 {
             private double[] x1x2 = new double[] {0, 0};
@@ -23,10 +74,18 @@ namespace Myk {
                 return this.GetX1() * mat21.GetX1() + this.GetX2() * mat21.GetY1();
             }
 
+            public static double operator*(Matrix1x2 lhs, Matrix2x1 rhs) {
+                return lhs.DotProduct(rhs);
+            }
+
             public Matrix1x2 Multiply(double v) {
                 x1x2[0] = GetX1() * v;
                 x1x2[1] = GetX2() * v;
                 return this;
+            }
+
+            public override string ToString() {
+                return "{ " + GetX1() + ", " +GetX2() + " }";
             }
 
             public double GetX2() {
@@ -50,34 +109,109 @@ namespace Myk {
                 return x1y1[1];
             }
 
+            public Matrix2x1 add(double v) {
+                return new Matrix2x1(new double[] { GetX1() + v, GetY1() + v });
+            }
+
+            public Matrix2x1 add(Matrix2x1 v) {
+                return new Matrix2x1(new double[] { GetX1() + v.GetX1(), GetY1() + v.GetY1() });
+            }
+
             public Matrix2x1 Multiply(double v) {
-                x1y1[0] = GetX1() * v;
-                x1y1[1] = GetY1() * v;
-                return this;
+                var x1 = GetX1() * v;
+                var y1 = GetY1() * v;
+                return new Matrix2x1(new double[] { x1, y1 });
+            }
+            public override string ToString() {
+                return "{ " + GetX1() + ", " +GetY1() + " }";
+            }
+
+            public static Matrix2x1 operator+(Matrix2x1 lhs, Matrix2x1 rhs) {
+                return lhs.add(rhs);
+            }
+
+            public static Matrix2x1 operator+(Matrix2x1 lhs, double rhs) {
+                return lhs.add(rhs);
             }
 
         }
+#endregion
     } // Myk.Lib namespace end
 
     class Program {
+#region data
         const double E = 2.71828182846;
-
-        static readonly double[] 教師データ = { 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D};
-        const double b = 1; // バイアス
+        static readonly double[] 教師データ = { 
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D,
+            0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D, 0D
+    };
+#endregion
+        // 初期バイアス
+        static double bias = 1; 
+        // 初期重み
+        static Matrix2x1 weightW1W2 = new Matrix2x1(new double[] {1D, 1D}).Multiply(学習率);
+        // 学習率
         const double 学習率 = 0.5D;
-        static readonly Matrix2x1 weightW1W2 = new Matrix2x1(new double[] {1D, 1D}).Multiply(学習率);
+        // 初期入力(x1, x2)
+        static readonly Matrix1x2 paraX1X2 = new Matrix1x2(new double[] { 1D, 1D });
 
-        static int StepFunc(double x) {
-            return (int)(x / Math.Abs(x));
+        // 活性化関数（ステップ関数）
+        public static int StepFunc(double x) {
+            /* xを絶対値で割ると1か-1になり+1をすると0か2になる ÷2をすると0か1になり、ステップ関数になる */
+            return (int)((x / Math.Abs(x)) + 1) / 2;
         }
 
+        public static double Sigmoid(double x) {
+            return 1 / (1 + Math.Pow(x, E));
+        }
+
+        public static double Affine(Matrix1x2 x, Matrix2x1 w, double bias) {
+            return x * w + bias;
+        }
+
+        static void Routine(Matrix2x1 weight, double bias, int count) {
+            if (0 >= count) return;
+            if (教師データ.Length < count) Console.WriteLine(
+                "学習回数が多すぎます。/n学習データは " + 教師データ.Length + " 個なのでそれ以下に設定してください。");
+            double output = StepFunc(Affine(paraX1X2, weight, bias));
+            var dE = 教師データ[教師データ.Length - count] - output;
+            var dWeight = weight.Multiply(dE * 学習率);
+            var dBias = dE * 学習率 * bias;
+            var newWeight = dWeight + weight;
+            var newBias = dBias + bias;
+            Console.WriteLine("出力= " + output);
+            Console.WriteLine("dE= " + dE);
+            Console.WriteLine("dWeight= " + dWeight);
+            Console.WriteLine("dBias= " + dBias);
+            Console.WriteLine("newWeight= " + newWeight);
+            Console.WriteLine("newBias= " + newBias);
+            Console.WriteLine("================学習残り " + (count - 1) + " 回===========================");
+            Routine(newWeight, newBias, --count);
+        }
         [STAThread]
         static void Main() {
-            var paraX1X2 = new Matrix1x2(new double[] {1D, 1D});
-            double resultY = StepFunc(paraX1X2.DotProduct(weightW1W2) + b);
-            Console.WriteLine(resultY);
-            Application.Run(new Form1());
+            //Routine(weightW1W2, bias, 教師データ.Length);
+            Routine(weightW1W2, bias, 3);
+            //Application.Run(new Form1());
         }
+
+        static partial void HelloFrom(string name);
     }
 
 } // Myk namespace end
+#endif

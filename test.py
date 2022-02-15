cb7b27b09ccb753e4e670cc8cf2494d0da582b4d
miyukic <m.yukic10@gmail.com>
2022-02-15 19:50:56 +0900

    Pythonのクラス継承時の属性の変化を見る

--- test.py
+++ test.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
 
 class A:
+    a = 0
+    b = "A Class です"
 
-    def f(self):
-        print("変更される前の関数fです")
+class B(A):
+    pass
 
+print("__dict__keys() = ", B.__dict__.keys())
+print("dir() = ", dir(B))
+#class A:
 
-    @staticmethod
-    def g():
-        print("変更される前の関数gです")
-
-
-    @classmethod
-    def h(cls):
-        print("変更される前の関数hです")
-
-
-def foo(self):
-    print("変更された後の関数f")
+#    def f(self):
+#        print("変更される前の関数fです")
 
 
-def main():
-    a = A()
-    a.f = foo
-    a.f()
+#    @staticmethod
+#    def g():
+#        print("変更される前の関数gです")
 
 
-if __name__ == "__main__":
-    main()
+#    @classmethod
+#    def h(cls):
+#        print("変更される前の関数hです")
+
+
+#def foo(self):
+#    print("変更された後の関数f")
+
+
+#def main():
+#    a = A()
+#    a.f = foo
+#    a.f()
+
+
+#if __name__ == "__main__":
+#    main()

7d2eca9f99ac19c174e9e64ba1b5a0a80098263b
miyukic <m.yukic10@gmail.com>
2022-02-21 22:06:07 +0900

    DllExportTest追加

---
+++ DllExportTest/DllExportTest/LibraryTest/LibraryTest.cpp
@@ -0,0 +1,22 @@
+﻿// LibraryTest.cpp : DLL 用にエクスポートされる関数を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "LibraryTest.h"
+
+
+// これは、エクスポートされた変数の例です
+LIBRARYTEST_API int nLibraryTest=0;
+
+// これは、エクスポートされた関数の例です。
+LIBRARYTEST_API int fnLibraryTest(void)
+{
+    return 32;
+}
+
+// これは、エクスポートされたクラスのコンストラクターです。
+CLibraryTest::CLibraryTest()
+{
+    return;
+}
---
+++ DllExportTest/DllExportTest/LibraryTest/LibraryTest.h
@@ -0,0 +1,22 @@
+﻿// 以下の ifdef ブロックは、DLL からのエクスポートを容易にするマクロを作成するための
+// 一般的な方法です。この DLL 内のすべてのファイルは、コマンド ラインで定義された LIBRARYTEST_EXPORTS
+// シンボルを使用してコンパイルされます。このシンボルは、この DLL を使用するプロジェクトでは定義できません。
+// ソースファイルがこのファイルを含んでいる他のプロジェクトは、
+// LIBRARYTEST_API 関数を DLL からインポートされたと見なすのに対し、この DLL は、このマクロで定義された
+// シンボルをエクスポートされたと見なします。
+#ifdef LIBRARYTEST_EXPORTS
+#define LIBRARYTEST_API __declspec(dllexport)
+#else
+#define LIBRARYTEST_API __declspec(dllimport)
+#endif
+
+// このクラスは dll からエクスポートされました
+class LIBRARYTEST_API CLibraryTest {
+public:
+	CLibraryTest(void);
+	// TODO: メソッドをここに追加します。
+};
+
+extern LIBRARYTEST_API int nLibraryTest;
+
+LIBRARYTEST_API int fnLibraryTest(void);

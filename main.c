b698f6c7134d7314fd19f0730115d3cac55dea53
miyukic <m.yukic10@gmail.com>
2022-02-27 05:29:47 +0900

    update

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-void cfunc(void) {
-    puts("called cfunc");
+
+int main(void) {
+    printf("%f", 3.0);
+    return 0;
 }
+
+//void cfunc(void) {
+//    puts("called cfunc");
+//}
--- test2.cpp
+++ test2.cpp
@@ -1,1 +1,23 @@
-#include "test.hpp"
+#include <iostream>
+#include <string>
+
+class Hoge {
+    static Hoge _instance;
+public:
+    std::string name = "Hoge";
+    static Hoge& getInstance() {
+        //if (_instance == nullptr) {
+        //    _instance = new Hoge();
+        //}
+        //return _instance;
+        return _instance;
+    }
+
+};
+
+int main() {
+    Hoge& hoge = Hoge::getInstance();
+    std::cout << hoge.name;
+    return 0;
+}
+

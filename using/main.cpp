d3c1294094c6fa4a9978b76564d493fa1cf8f676
miyukic <m.yukic10@gmail.com>
2022-02-28 22:48:56 +0900

    update

---
+++ using/main.cpp
@@ -0,0 +1,12 @@
+#include "main.hpp"
+
+
+int main() {
+    using namespace hoge;
+    Hoge hoge; 
+    //std::vector<Hoge> vh = std::vector<Hoge>();
+    FugaAlias fa;
+    return 0;
+}
+
+
---
+++ using/main.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+namespace hoge {
+    class Hoge { };
+}
+
+namespace hoge {
+    using FugaAlias = std::vector<Hoge>;
+}
+

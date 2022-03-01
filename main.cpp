53c6719975a32de2e27cecd90d2963aafbe7396a
miyukic <m.yukic10@gmail.com>
2022-03-01 18:38:13 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
+#include <iostream>
 
-void Sub();
+class Hoge {
+    inline static int count = 0;
+    inline static int copycount = 0;
+public:
+    Hoge() {
+        count++;
+        std::cout << "Hoge " << count << std::endl;
+    }
+    Hoge (const Hoge& obj) {
+        copycount++;
+        std::cout << "Hogeコピーコンストラクタ " << copycount << std::endl;
+    }
+};
+
+Hoge hoge;
+
+Hoge& sub() {
+    return hoge;
+}
 
 int main() {
-    Sub();
+    Hoge& hoge = sub(); //値返しの関数の戻り値を参照型で受けることはできない。
+    return 0;
 }
 

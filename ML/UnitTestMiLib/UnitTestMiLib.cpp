7e4aa0a5acad09a71dc1f78a12ae5953cfdae6ca
miyukic <m.yukic10@gmail.com>
2022-03-02 08:44:38 +0900

    equalsのテストコード

--- ML/UnitTestMiLib/UnitTestMiLib.cpp
+++ ML/UnitTestMiLib/UnitTestMiLib.cpp
@@ -5,6 +5,12 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace myk::lib;
+#ifdef DBG
+#else
+#	define USE_STR using namespace std::literals::string_literals;
+#	define DBG(...) Logger::WriteMessage([&](){ USE_STR return #__VA_ARGS__ ## s + " = "s + std::to_string(__VA_ARGS__); }().c_str());
+#endif
+
 
 namespace UnitTestMiLib
 {
@@ -14,7 +20,7 @@ namespace UnitTestMiLib
 		
 		TEST_METHOD(Matrixクラスの＝演算子) {
 			std::vector<std::vector<double>> vec1 = { { 1, 2, 3 }, {2, 3, 4} };
-			std::vector<std::vector<double>> vec2 = { { 2, 2, 3 }, {2, 4, 4} };
+			std::vector<std::vector<double>> vec2 = { { 2, 2, 3.2 }, {2, 4.3, 4} };
 			//行列が同じ場合のチェック
 			Matrix mtx1{ vec1 };
 			Matrix mtx2( vec1 );
@@ -62,6 +68,40 @@ namespace UnitTestMiLib
 			Matrix answer({ {3.0 + a, 12.3 + a , 5.0 + a}, {2.0 + a, 4.3 + a, 5.0 + a},{4.0+a, 12.0+a, 123.0+a} });
 			bool result = answer == mtx2;
 			Assert::AreEqual(true, result);
+		}
+
+		TEST_METHOD(equals_API関数) {
+			double arr[] =
+			{
+				1, 2, 3, 4,
+				1, 2, 3, 4,
+				1, 2, 3, 4
+			};
+			myk::ID id = initNativeMatrix(arr, 0, 3, 4);
+			myk::ID id2 = initNativeMatrix(arr, 0, 3, 4);
+			bool result = equals(id, id2);
+			Assert::AreEqual(true, result);
+			//Logger::WriteMessage((std::to_string(result)).c_str());
+			DBG(result);
+			double arr2[] =
+			{
+				9, 4, 3.0, 4.00,
+				8, 2, 11 , 4.2 ,
+				3, 2, 3.3, 6.12
+			};
+			myk::ID id3 = initNativeMatrix(arr2, 0, 3, 4);
+			bool result2 = equals(id, id3);
+			Assert::AreEqual(false, result2);
+			double arr3[] =
+			{
+				9, 4, 3.0, 4.00,
+				8, 2, 11 , 4.2 ,
+				3, 2, 3.3, 6.12,
+				3, 2, 3.3, 6.12
+			};
+			myk::ID id4 = initNativeMatrix(arr3, 0, 4, 4);
+			bool result3 = equals(id, id4);
+			Assert::AreEqual(false, result3);
 		}
 
 	};

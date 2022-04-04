f28f7bbb629f700055f2934ca0a911d45377c568
miyukic <m.yukic10@gmail.com>
2022-04-05 03:14:35 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
+#include <string>
+#include <cstdint>
 
-class Hoge {
-    inline static int count = 0;
-    inline static int copycount = 0;
-public:
-    Hoge() {
-        count++;
-        std::cout << "Hoge " << count << std::endl;
-    }
-    Hoge (const Hoge& obj) {
-        copycount++;
-        std::cout << "Hogeコピーコンストラクタ " << copycount << std::endl;
-    }
+
+//class Hoge {
+//    inline static int count = 0;
+//    inline static int copycount = 0;
+//public:
+//    Hoge() {
+//        count++;
+//        std::cout << "Hoge " << count << std::endl;
+//    }
+//    Hoge (const Hoge& obj) {
+//        copycount++;
+//        std::cout << "Hogeコピーコンストラクタ " << copycount << std::endl;
+//    }
+//};
+
+//Hoge hoge;
+//
+//Hoge& sub() {
+//    return hoge;
+//}
+
+
+struct Sample {
+    std::string username;
+    std::string email;
+    uint64_t signInCount;
+    bool active;
 };
 
-Hoge hoge;
-
-Hoge& sub() {
-    return hoge;
-}
-
 int main() {
-    Hoge& hoge = sub(); //値返しの関数の戻り値を参照型で受けることはできない。
+    Sample sample = Sample { "miyuki", "hogehoge@example.com", 0, true };
     return 0;
 }
 
--- rust/kazuate/guessing_game/src/main.rs
+++ rust/kazuate/guessing_game/src/main.rs
@@ -1,13 +1,38 @@
-enum IpAddrKind {
-    V4,
-    V6,
+//enum IpAddrKind {
+//    V4,
+//    V6,
+//}
+
+//fn sub() {
+//    let four = IpAddrKind::V4;
+//    println!("Hello Sub!!");
+//}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
-fn sub() {
-    let four = IpAddrKind::V4;
-    println!("Hello Sub!!");
+let user1 = User {
+    username: String::from("miyuki"),
+    email: String::from("hogehoge@example.com"),
+    sign_in_count: 0,
+    active: true,
+}
+
+fn func<'s>(val: &'s i32) -> &'s i32 {
+    val
+}
+
+fn 出力する<'s>(msg: &'s String) {
+    println!("{}", msg);
 }
 
 fn main() {
-    sub();
+    let message = String::from("こんにちは");
+    出力する(&message);
+    let val = 20;
+    func(&val);
 }

67de3088879e69de78f20bd1a442a3398e15e051
miyukic <m.yukic10@gmail.com>
2022-06-02 02:44:51 +0900

    update

--- TimerTest2.cpp
+++ TimerTest2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <chrono>
 #include <thread>
 #include <iomanip>
@@ -7,9 +6,9 @@
 
 int main() {
     double total_time = 0.00;
-    for (std::size_t i = 0; i < 1000; ++i ) {
+    for (std::size_t i = 0; i < 100000; ++i ) {
         auto start = std::chrono::system_clock::now();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //1ms待機
+        std::this_thread::sleep_for(std::chrono::milliseconds(0)); //1ms待機
         auto end = std::chrono::system_clock::now();
         total_time += static_cast<double>(
                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0);

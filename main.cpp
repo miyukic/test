0f6e13d23ac6ca00393c1ea90e8239f735430712
miyukic <m.yukic10@gmail.com>
2022-06-27 00:57:50 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,41 +1,34 @@
 #include <iostream>
-#include <string>
-#include <cstdint>
+//*#include <string>
+//*#include <cstdint>
+#include <memory>
 
-
+//Hogeclass Hoge {
+//Hogepublic:
+//Hoge    int a = 0;
+//Hoge};
+//Hoge
+//Hoge
+//Hogestd::unique_ptr<Hoge> f() {
+//Hoge    return std::make_unique<Hoge>();
+//Hoge}
+//Hoge
+//Hogeint main() {
+//Hoge    auto a = f();
+//Hoge} 
 
 class Hoge {
-   inline static int count = 0;
-   inline static int copycount = 0;
-public:
-   Hoge() {
-       count++;
-       std::cout << "Hoge " << count << std::endl;
-   }
-   Hoge (const Hoge& obj) {
-       copycount++;
-       std::cout << "Hogeコピーコンストラクタ " << copycount << std::endl;
-   }
-   static bool operator==(const Hoge& rhs) const& {
-       return true;
-   }
 };
 
-//Hoge hoge;
-
-
-//Hoge& sub() {
-//   return hoge;
-//}
-
-
-// struct Sample {
-//     std::string username;
-//     std::string email;
-//     uint64_t signInCount;
-//     bool active;
-// };
+std::unique_ptr<Hoge> hoge() {
+    std::unique_ptr<Hoge> p(new Hoge());
+    return p;
+    //return std::move(make_unique<int>());
+}
 
 int main() {
+    using namespace std;
+    std::unique_ptr<Hoge> hoge = ::hoge();
+    return 0;
 }
 

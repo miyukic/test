e38e86dd4a6e290d0bf6a5e2775e4976ecc4b3dd
miyukic <m.yukic10@gmail.com>
2022-07-05 00:32:23 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 #include <iostream>
-//*#include <string>
-//*#include <cstdint>
-#include <memory>
 
-//Hogeclass Hoge {
-//Hogepublic:
-//Hoge    int a = 0;
-//Hoge};
-//Hoge
-//Hoge
-//Hogestd::unique_ptr<Hoge> f() {
-//Hoge    return std::make_unique<Hoge>();
-//Hoge}
-//Hoge
-//Hogeint main() {
-//Hoge    auto a = f();
-//Hoge} 
 
-class Hoge {
-};
-
-std::unique_ptr<Hoge> hoge() {
-    std::unique_ptr<Hoge> p(new Hoge());
-    return p;
-    //return std::move(make_unique<int>());
-}
-
-int main() {
-    using namespace std;
-    std::unique_ptr<Hoge> hoge = ::hoge();
-    return 0;
+int main(int argc, char* argv[]) {
+    std::cout << argc << std::endl;
+    if ( argc >= 2 ) {
+        std::cout << argv[1][1] << std::endl;
+    }
 }
 

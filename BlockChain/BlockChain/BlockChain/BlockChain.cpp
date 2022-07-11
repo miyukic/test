d454e3f0385e374295a88b468e4a98daba6eba11
miyukic <m.yukic10@gmail.com>
2022-07-12 02:18:56 +0900

    add BlockChain CMake Proj

---
+++ BlockChain/BlockChain/BlockChain/BlockChain.cpp
@@ -0,0 +1,14 @@
+﻿// BlockChain.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "BlockChain.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::string input;
+	std::cin >> input;
+	return 0;
+}

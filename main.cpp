e08273e1f5dc198e7eedcb2c59753953ce38ec5d
miyukic <m.yukic10@gmail.com>
2022-08-22 23:33:03 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 
 
 int main(int argc, char* argv[]) {
-    //std::cout << argc << std::endl;
-    if ( argc >= 2 ) {
-        std::cout << argv[1][1] << std::endl;
-    }
-    int a[5] = {1, 2, 3, 4, 5};
-    std::cout << *1[&a] << std::endl; //1が表示される
-    std::cout << 0[a] << std::endl; //1が表示される
-    // std::cout << *(0 + 1); //1が表示される
-    // std::cout << **(0 + 1); //1が表示される
-    std::cout << *(*(&a) + 0) << std::endl;　//2
-    std::cout << *(0[&a] + 0) << std::endl;   //2
+    std::string hoge = "hogehoge";
+    std::string aiueo = "あいうえお";
+    std::cout << hoge << std::flush;
+    std::cout << aiueo << std::endl;
 }
 

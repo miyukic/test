9804c18a664b7a4acf35e0e190c0c29763582c0d
miyukic <m.yukic10@gmail.com>
2023-01-10 22:52:06 +0900

    update

---
+++ char/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string charName;
+    std::cout << "キャラクターの名前を入力してください。" << std::endl;
+    std::cin >> charName;
+}
--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
-#include <cstdio>
+#include <vector>
+#include <ranges>
 
-template<std::size_t N, double R = 0.1> struct Sum {
-    static constexpr double value = R + Sum<N-1>::value;
-};
-
-template<> struct Sum<0> {
-    static constexpr double value = 0.0;
-};
 
 int main() {
-
-    std::printf("%.16f", Sum<1000>::value);
-
-    double sum = 0.0;
-    for (size_t i = 0; i < 1000; ++i) {
-        sum += 0.1;
+    std::vector<int> v(10);
+    std::ranges::fill(std::ranges::begin(v) + 3, std::ranges::end(v), 5);
+    for (const auto &n : v) {
+        std::cout << n << std::endl;
     }
-    std::printf("%.16f",sum);
     return 0;
 }

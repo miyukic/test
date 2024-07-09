4a81dfbbc8503c74765f85a7d1df63e49dfd654e
miyukic <m.yukic10@gmail.com>
2024-07-10 01:19:04 +0900

    update

---
+++ cs/cs2/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+class Program {
+
+    public static void Main(string[] args) {
+        
+        Func<double, double> f = (x) => Math.Pow(x, 2) + 2 * x;
+        for (uint i = 0; i < 11; ++i) {
+            Console.WriteLine(f((double)i));
+        }
+    }
+}
